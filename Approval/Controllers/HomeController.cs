0b93a78d86f0773547e38912a40e6de0a16f713f
Staia <staiascharp@gmail.com>
2022-11-30 12:14:22 +0100

    IDbConnection21

--- Approval/Controllers/HomeController.cs
+++ Approval/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using Approval.Models;
+using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,10 +16,20 @@ namespace Approval.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration; 
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration; 
+        }
+
+        public IDbConnection Connect
+        {
+            get
+            {
+                return new SqlConnection("Server = localhost\\SQLEXPRESS;Database = Orders;Trusted_Connection = True;");
+            }
         }
 
         public IActionResult Index()
@@ -25,8 +39,11 @@ namespace Approval.Controllers
 
         public IActionResult Privacy()
         {
-            return View();
+            PageData privacyTable = new PageData(Connect);
+            return View(privacyTable);
         }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Approval/Models/ListOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Approval.Models
+{
+    public enum Status
+    {
+        Create,
+        HeadOfDepartment,
+        HeadOfITDepartment,
+        GeneralManager,
+        Orded
+    }
+
+    public class ListOrder
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public string Status { get; set; }
+        public DateTime Created { get; set; }
+        public string CreatedBy { get; set; }
+        public string Category { get; set; }
+        public string NumberFromERP { get; set; }
+    }
+}
