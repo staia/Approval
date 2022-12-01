36d08c2d9973768ae85230a330e358f388008918
ASUS <ASUS@DESKTOP-99BJBI2>
2022-12-01 10:56:20 +0200

    Подключился к базе через SqlConnection

--- Approval/Controllers/HomeController.cs
+++ Approval/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using Approval.Models;
 using Approval.Views.Home;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,12 +16,20 @@ namespace Approval.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
-
+        public IDbConnection Connext
+        {
+            get
+            {
+                return new SqlConnection(_configuration.GetConnectionString("ConnectToMyDataBese"));
+            }
+        }
 
 
 
@@ -27,12 +38,12 @@ namespace Approval.Controllers
 
         public IActionResult Index()
         {
-            return View("Privacy");
+            return View();
         }
         [HttpPost]
         public IActionResult Autorize(RegisterAtUser userdata)
         {
-            UserData user = new UserData(Connect); // проверять userdata 
+            UserData user = new UserData(Connext); // проверять userdata 
 
 
             if (userdata.Password != null)
--- Approval/Models/ListOrder.cs
+++ Approval/Models/ListOrder.cs
@@ -12,11 +12,9 @@ namespace Approval.Models
         public string UserName { get; set; }
         public string Password { get; set; }
     }
-    public class AnRegisterUser
+    public class AnRegisterUser : RegisterAtUser
     { 
-        public string FullName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public string ConfirmPassword { get; set; }
 
     }
