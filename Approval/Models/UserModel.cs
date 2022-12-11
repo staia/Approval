e80247d57914f98e016d2fe9828d72895e476aab
Staia <staiascharp@gmail.com>
2022-12-11 17:09:55 +0100

    NullVersion

--- Approval/Models/UserModel.cs
+++ Approval/Models/UserModel.cs
@@ -12,6 +12,7 @@ namespace Approval.Models
         public string Username { get; set; }
         [Required]
         public string Password { get; set; }   
+
         
         public string Role { get; set; }    
     }
